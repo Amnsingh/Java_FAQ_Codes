45f8a6f0775eceb33fe555537bcd41ffd7638078
Amnsingh <amnsingh36@gmail.com>
2024-09-05 01:39:54 -0400

    Method one to swap 2 numbers

--- FAQ_Java_Codes/JP01_Swap_2_Numbers.java
+++ FAQ_Java_Codes/JP01_Swap_2_Numbers.java
@@ -1,4 +1,14 @@
 package FAQ_Java_Codes;
 
 public class JP01_Swap_2_Numbers {
+    public static void main(String args[]){
+        //Logic1- Third Variable
+        int a=5, b=10;
+        System.out.println("Before Swapping "+" a: "+a +" AND"+" b: "+b);
+        int t=a;
+        a=b;
+        b=t;
+
+        System.out.println("After Swapping "+" a: "+a +" AND"+"b: "+b);
+    }
 }
